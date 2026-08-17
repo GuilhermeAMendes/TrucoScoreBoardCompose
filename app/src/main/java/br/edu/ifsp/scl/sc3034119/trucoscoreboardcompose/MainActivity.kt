377c57d5559ff9b86aefe4b8d7c0f7776e47b363
Guilherme Mendes <guilherme.andmendes03@gmail.com>
2026-08-16 23:04:14 -0300

    feat: create the application header

--- app/src/main/java/br/edu/ifsp/scl/sc3034119/trucoscoreboardcompose/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3034119/trucoscoreboardcompose/MainActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import br.edu.ifsp.scl.sc3034119.trucoscoreboardcompose.ui.theme.TrucoScoreBoardComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,12 +25,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             TrucoScoreBoardComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text(
-                        text = "Iniciando projeto",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    TrucoScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
+}
+
+@Composable
+fun TrucoScreen(modifier: Modifier = Modifier){
+    Text(
+        text = "Placar da Partida",
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TrucoScreenPreview(){
+    TrucoScreen(modifier = Modifier)
 }
